5a79c44a6703ed65951cbc19bbacac5917d435a4
B9R9 <baptiste.riffard@gmail.com>
2024-09-05 12:56:17 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { renderMiddleware } from "./middlewares/renderMiddleware.js";
+
+const app = new Application();
+const router = new Router();
+
+let visitsCount = 0;
+const countVisitsMiddleware = async (context, next) => {
+	if (context.request.url.pathname === "/visits") {
+		visitsCount++;
+	}
+	console.log(visitsCount);
+	await next();
+};
+
+const notFoundMiddleware = async (context, next) => {
+	await next();
+	if (context.response.status === 404) {
+	  context.response.status = 404;
+	  context.response.body = "Nothing here yet.";
+	}
+};
+  
+app.use(renderMiddleware);
+app.use(countVisitsMiddleware);
+
+
+router.get("/", ({ render }) => render("index.eta"));
+router.get("/visits", ({ render }) => render("visits.eta", { visitsCount }));
+router.get("/meaning", ({ render }) => render("meaning.eta"));
+
+app.use(router.routes());
+app.use(notFoundMiddleware);
+
+if (!Deno.env.get("TEST_ENVIRONMENT")) {
+  app.listen({ port: 7777 });
+}
+
+export default app;
